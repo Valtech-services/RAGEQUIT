3883a3f78341e440875180ef68b038cdb1af5137
Valtech-services <valtech.services@proton.me>
2026-06-02 14:40:28 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './styles/tokens.css'
 import './styles/animations.css'
 import './styles/global.css'
+import { AuthProvider } from './context/AuthContext'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>
 )
