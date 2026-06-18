1c1830c6cb8a30364de363f13bb7fee4171d09cc
Valtech-services <valtech.services@proton.me>
2026-06-19 01:12:20 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,10 @@ import './styles/animations.css'
 import './styles/global.css'
 import './i18n/i18n'
 import { AuthProvider } from './context/AuthContext'
+import { initGA } from './lib/analytics-ga'
 import App from './App.jsx'
+
+initGA()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
