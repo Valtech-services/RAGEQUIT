902a05fd5e0f3dd1bef76b6d08fb057c73e6aaa0
Valtech-services <valtech.services@proton.me>
2026-09-03 01:31:47 +0200

    Update GameCard.jsx

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -41,8 +41,15 @@ export default function GameCard({ game, size = 'medium', source = 'bento' }) {
       {/* Badges statiques (immobiles au survol) */}
       {game.hot && (
         <span className="gamecard__hot">
-          <svg className="gamecard__hot-icon" viewBox="0 0 24 24" fill="none">
-            <path d="M12 2c1 3-1 5-2 6-1.5 1.5-3 3-3 6a5 5 0 0010 0c0-2-1-3.5-2-5 2 1 3 3 3 5a7 7 0 11-14 0c0-5 5-7 8-12z" fill="#00b4e6"/>
+          <svg className="gamecard__hot-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <defs>
+              <linearGradient id="hotFlame" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="0%" stop-color="#5ee0ff"/>
+                <stop offset="100%" stop-color="#0088cc"/>
+              </linearGradient>
+            </defs>
+            <path d="M13.5 2c.5 3.5-1.5 5-3 7.5-1 1.7-1 3.5.5 4.7.4-1.3.3-2.7 1.4-3.7-.2 2.3 1.3 3.4 2 4.6.8 1.4.4 3.1-.9 4.2 3-1 5-3.6 5-6.8 0-3.3-2.3-5-3.2-7.4-.5 1.4-1.6 2-2.6 2.6C15.9 6 15.4 3.6 13.5 2z" fill="url(#hotFlame)"/>
+            <path d="M9.5 12.5c-1.2 1-1.9 2.4-1.9 4A4.4 4.4 0 0012 21c-1.3-1.1-1.7-2.8-.9-4.2-.9-1.2-1.6-2.4-1.6-4.3z" fill="#aef0ff"/>
           </svg>
         </span>
       )}
