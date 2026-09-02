a9551aa9e9f141ef5b2bdac7c03dfd42ac230ff0
Valtech-services <valtech.services@proton.me>
2026-09-03 01:38:29 +0200

    Update GameCard.jsx

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -43,13 +43,18 @@ export default function GameCard({ game, size = 'medium', source = 'bento' }) {
         <span className="gamecard__hot">
           <svg className="gamecard__hot-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <defs>
-              <linearGradient id="hotFlame" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="0%" stop-color="#5ee0ff"/>
-                <stop offset="100%" stop-color="#0088cc"/>
+              <linearGradient id="hotFlame" x1="12" y1="2" x2="12" y2="22" gradientUnits="userSpaceOnUse">
+                <stop offset="0%" stop-color="#ffd23f"/>
+                <stop offset="45%" stop-color="#ff8a00"/>
+                <stop offset="100%" stop-color="#e02200"/>
+              </linearGradient>
+              <linearGradient id="hotFlameCore" x1="12" y1="9" x2="12" y2="21" gradientUnits="userSpaceOnUse">
+                <stop offset="0%" stop-color="#fff3b0"/>
+                <stop offset="100%" stop-color="#ffb020"/>
               </linearGradient>
             </defs>
-            <path d="M13.5 2c.5 3.5-1.5 5-3 7.5-1 1.7-1 3.5.5 4.7.4-1.3.3-2.7 1.4-3.7-.2 2.3 1.3 3.4 2 4.6.8 1.4.4 3.1-.9 4.2 3-1 5-3.6 5-6.8 0-3.3-2.3-5-3.2-7.4-.5 1.4-1.6 2-2.6 2.6C15.9 6 15.4 3.6 13.5 2z" fill="url(#hotFlame)"/>
-            <path d="M9.5 12.5c-1.2 1-1.9 2.4-1.9 4A4.4 4.4 0 0012 21c-1.3-1.1-1.7-2.8-.9-4.2-.9-1.2-1.6-2.4-1.6-4.3z" fill="#aef0ff"/>
+            <path d="M12 2c.6 2.9-.7 4.6-2 6.2-1.4 1.7-2.8 3.4-2.8 6A4.8 4.8 0 0012 19c2.9 0 5.2-2.2 5.2-5 0-2.2-1-3.6-2-5 .1 1.1-.3 2-1.2 2.6.5-2.4-.3-4.3-1.6-5.8C12.9 4.6 12.6 3.2 12 2z" fill="url(#hotFlame)"/>
+            <path d="M12 11c-.9 1-1.5 2.1-1.5 3.4A2.7 2.7 0 0012 17c1.3 0 2.4-1 2.4-2.4 0-1.1-.5-1.8-1.1-2.6-.1.7-.4 1.2-1 1.6.3-1.2-.1-2-1.3-2.6z" fill="url(#hotFlameCore)"/>
           </svg>
         </span>
       )}
